90770cf882927c938864ef1bf06f828ed8b502df
Kevin Languasco <kevin.languasco@pucp.pe>
2016-07-23 01:08:38 -0500

    Week 2

--- 02-polymorphism/HW02.hs
+++ 02-polymorphism/HW02.hs
@@ -26,11 +26,11 @@ colors = [Red, Green, Blue, Yellow, Orange, Purple]
 exactMatches :: Code -> Code -> Int
 exactMatches (x:xs) (y:ys) = (if x==y then 1 else 0) + exactMatches xs ys
 exactMatches _ _ = 0
-        
+
 
 -- Exercise 2 -----------------------------------------
 
--- For each color, count how many times it's repeated in xs 
+-- For each color, count how many times it's repeated in xs
 countColors :: Code -> [Int]
 countColors xs = map (countAColor xs) colors
     where countAColor :: [Peg] -> Peg -> Int
@@ -44,27 +44,38 @@ matches xs ys = foldl' (+) 0 (zipWith min (countColors xs) (countColors ys))
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove secret guess = Move guess (exactMatches secret guess) (matches secret guess - exactMatches secret guess)
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent (Move guess x y) secret = getMove secret guess == Move guess x y
 
 -- Exercise 5 -----------------------------------------
 
 filterCodes :: Move -> [Code] -> [Code]
-filterCodes = undefined
+filterCodes m = filter (isConsistent m)
 
 -- Exercise 6 -----------------------------------------
 
 allCodes :: Int -> [Code]
-allCodes = undefined
+allCodes n
+    | n <= 0 = [[]]
+    | otherwise = concatMap newCodes (allCodes (n-1))
+        where newCodes :: Code -> [Code]
+              newCodes a = map (:a) colors
 
 -- Exercise 7 -----------------------------------------
 
 solve :: Code -> [Move]
-solve = undefined
+solve secret = nextMove filteredCodes
+    where initialGuess = replicate (length secret) Red
+          filteredCodes = filterCodes (getMove secret initialGuess) (allCodes (length secret))
+          nextMove :: [Code] -> [Move]
+          nextMove [] = []
+          nextMove (guess:availableCodes)
+            | guess==secret = [getMove secret guess]
+            | otherwise = getMove secret guess : nextMove availableCodes
 
 -- Bonus ----------------------------------------------
 
